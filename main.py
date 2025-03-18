da12cee510cc88907ce033359592d921125e694c
Bufonlol <orkurito868@gmail.com>
2025-03-18 04:59:15 -0600

    fe

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ import os
 
 app = FastAPI()
 
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todos los orígenes, puedes especificar tu frontend aquí
+    allow_methods=["*"],  # Permitir todos los métodos HTTP
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
+
 # Cargar modelos preentrenados utilizando joblib
 encoder = joblib.load("encoder.pkl")
 modelo = joblib.load("modelo_svc.pkl")
