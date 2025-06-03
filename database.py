6d8049cbeaf58c3180013facc6e1fe02f81b768a
Bufonlol <20223l001028@utcv.edu.mx>
2025-06-03 09:30:23 -0600

    Base de datos

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://sirade_database_0s0b_user:yPAIgoHn2R2kt3oIJkhkJLDLKPkOOqPd@dpg-cv8kep0gph6c73acia50-a.oregon-postgres.render.com:5432/sirade_database_0s0b"
+DATABASE_URL = "postgresql://sirade_bd_user:HSRipcmtYKo5QsP7sr5D9IDaYz6zOYIk@dpg-d0vh9ie3jp1c73e6usb0-a/sirade_bd"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
