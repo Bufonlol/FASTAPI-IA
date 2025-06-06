6b6d69ecb29f1d32795ee95de1d8331acbae63fa
Bufonlol <20223l001028@utcv.edu.mx>
2025-06-06 01:27:06 -0600

    Base de datos

--- auth.py
+++ auth.py
@@ -5,14 +5,15 @@ from jose import JWTError, jwt
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 SECRET_KEY = "7Xp4Mv2bT5qYt8wZzR9kA1cF3eD6gHjJ7LmN0oP4iK5lB8uV2nC9xQ3wE5rT6yU"
-ALGORITHM = "HS256"
+ALGORITHM = "HS384"
 
-def get_current_user_id(token: str = Depends(oauth2_scheme)) -> int:
+def get_current_user_id(token: str = Depends(oauth2_scheme)):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS384"])
         user_id = payload.get("userId")
         if user_id is None:
             raise HTTPException(status_code=401, detail="Token inválido: userId faltante")
-        return int(user_id)
-    except JWTError:
+        return user_id
+    except JWTError as e:
+        print("Error al decodificar token:", e)
         raise HTTPException(status_code=401, detail="Token inválido")
