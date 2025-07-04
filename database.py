d8d03b6b818571e57169f414d666487ae6c861db
Alan Pro <orkurito868@gmail.com>
2025-07-04 08:21:06 -0600

    V3.2 FASTSIRADE

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://sirade_bd_user:HSRipcmtYKo5QsP7sr5D9IDaYz6zOYIk@dpg-d0vh9ie3jp1c73e6usb0-a.oregon-postgres.render.com/sirade_bd"
+DATABASE_URL = "postgresql://sirade_bd_q5nv_user:1yseUfaHhS0dvGMjvVBe8JJnyeRUnkm4@dpg-d1jskvp5pdvs73fcetsg-a.oregon-postgres.render.com/sirade_bd_q5nv"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
